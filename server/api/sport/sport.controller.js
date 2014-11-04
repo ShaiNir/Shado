d89d13dd783b42ae5fb53763db321a6ba1ce7eab
Sammyfrw <sammyfrw@gmail.com>
2014-11-04 14:29:02 +0700

    Add csvtojson to read csv

--- server/api/sport/sport.controller.js
+++ server/api/sport/sport.controller.js
@@ -2,6 +2,15 @@
 
 var _ = require('lodash');
 var Sport = require('../models').Sport;
+var fs = require('fs')
+var output = []
+
+var Converter=require('csvtojson').core.Converter;
+var Player = require('../models').Player;
+var async = require('async')
+var path = require('path')
+
+
 
 // Get list of sports
 exports.index = function(req, res) {
@@ -60,6 +69,59 @@ exports.destroy = function(req, res) {
     });
 };
 
+//Populates a sport's list of default players
+exports.populate = function(req, res) {
+    Sport.find(req.params.id).then(function (sport) {
+        if(!sport) { return res.send(404); }
+        parseCsv(sport)
+        return res.send(201, sport);
+    }, function(error){
+        return handleError(res, error);
+    });
+}
+
 function handleError(res, error) {
     return res.send(500, error);
+}
+
+function parseCsv(sport) {
+    var directory = './default_players'
+    fs.readdir(directory, function(err, files) {
+        if (!files.length) {
+            return console.log("No files found in default_players");
+        }
+        async.map(files, function(file, callback) {
+            path.join(drectory, file);
+            var fileStream = fs.createReadStream(file);
+            var csvConverter = new Converter({constructResult:true});
+            csvConverter.on("end_parsed",function(jsonObj){
+                var players = populateDatabase(jsonObj, sport);
+            });
+        fileStream.pipe(csvConverter);
+        callback();
+        });
+    });
+}
+
+function populateDatabase(players, sport) {
+    async.each(players, function(player, callback) {
+        Player.findOrCreate({
+            where: {
+                name: player.name,
+                salary: player.salary,
+                realWorldTeam: player.realWorldTeam,
+                contractExpires: player.contractExpires
+            }
+        }).success(function(player, created){
+            player.setSport(sport);
+            player.save();
+        })
+    callback();
+    }, function(err){
+        if(err) {
+            console.log("Failed to process a player");
+        } else {
+            console.log("Successfully processed all players.");
+        }
+    });
 }
