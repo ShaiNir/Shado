783945ab12a38933afe8f86b21249720c87d26bd
Shai Nir <shainir123@gmail.com>
2014-12-09 11:19:53 -0500

    Adding some validation to-dos

--- server/api/models/player.js
+++ server/api/models/player.js
@@ -1,9 +1,9 @@
 module.exports = function(sequelize, DataTypes) {
     return sequelize.define('Player', {
-        name: DataTypes.STRING,
+        name: DataTypes.STRING, //Todo: validate sane for SQL/HTML
         defaultSalary: DataTypes.INTEGER,
         contractExpires: DataTypes.DATE,
         statsFeed: DataTypes.STRING,
-        realWorldTeam: DataTypes.STRING
+        realWorldTeam: DataTypes.STRING //Todo: validate sane for SQL/HTML
     })
 }
