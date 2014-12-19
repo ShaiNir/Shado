fb8e92301fbfa6df3a26ee4f0406e2864090948a
Shai Nir <shainir123@gmail.com>
2014-12-19 15:51:34 -0500

    Switching some error logging

--- server/app.js
+++ server/app.js
@@ -4,11 +4,13 @@
 
 'use strict';
 
+var logger = require('./logger')
+
 // Set default node environment to development
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 process.on('uncaughtException', function (err) {
-    console.log(err);
+    logger.error(err);
 });
 
 var express = require('express');
